d6f771b803f9cfa295fcb22f54ce182a788489f1
ivankstro7 <ivankstro7@gmail.com>
2022-02-08 19:08:12 -0500

    update

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const api = 'https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=6d7ac337bb80e4a6071f4816f5fe5c24';
-console.log(api);
-
+//api
+const api = 'https://api.openweathermap.org/data/2.5/weather?appid=6d7ac337bb80e4a6071f4816f5fe5c24&q=';
+console.log(api)
+// ID contenedor padre
 const weather = document.querySelector('#weather');
-console.log(weather);
 
-window.fetch(api)
-.then((respuesta) => respuesta.json())
-.then(responseJson => {
-    responseJson((item) => {                
+const input = document.getElementById('input');
+
+function btn () {
+    
+}
+
+window.fetch(api + "cali")
+    .then((respuesta) => respuesta.json())
+    .then(responseJson => {
+    const todosLosItems = [];             
         //div clima
         const containerClima = document.createElement("p");
-        containerClima.textContent = item.timezone;       
+        containerClima.textContent = responseJson.main.temp;        
 
-    });   
-    weather.append(containerClima);
-});
+        todosLosItems.push(containerClima);
+
+    weather.append(...todosLosItems)
+});         
